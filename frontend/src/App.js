9185a776e43bb113daa6cd42f0751d9b9acf0e80
cuong <cuong1042k1@gmail.com>
2022-09-30 08:39:53 +0700

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Shop from "./pages/Shop";
 import Product from "./pages/Product";
-import BlogDetail from './pages/BlogDetail'
+import BlogDetail from "./pages/BlogDetail";
 import LoginPage from "./pages/LoginPage";
 import Register from "./components/AuththenComponent/Register";
 import ContactPage from "./pages/ContactPage";
@@ -14,12 +14,11 @@ function App() {
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/blog" element={<Blog></Blog>}></Route>
       <Route path="/shop/detail/:id" element={<Product></Product>}></Route>
-      <Route path="/blog/detail/:id" element={<BlogDetail/>}/>
-      <Route path="/shop" element={<Shop/>}></Route>
-      <Route path="/login" element={<LoginPage/>}></Route>
-      <Route path="/register" element={<Register/>}></Route>
-      <Route path="/contact" element={<ContactPage/>}></Route>
-
+      <Route path="/blog/detail/:id" element={<BlogDetail />} />
+      <Route path="/shop" element={<Shop />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/contact" element={<ContactPage />}></Route>
     </Routes>
   );
 }
