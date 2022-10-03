106ee658662b5f5015a3cd8dc3251ef4982a7f8b
NGUYỄN VĂN THUẬN <MY ASUS@NGUYENVANTHUAN-2002-QUANGNAM.THUAN22102002>
2022-10-03 15:02:09 +0700

    profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,9 @@ import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import BlogDetail from './pages/BlogDetail'
 import LoginPage from "./pages/LoginPage";
-// import Register from "./components/AuththenComponent/Register";
+import Register from "./components/AuththenComponent/Register";
 import ContactPage from "./pages/ContactPage";
+import ProfilePage from "./pages/ProfilePage";
 // chứa các trang
 function App() {
   return (
@@ -18,9 +19,11 @@ function App() {
       <Route path="/blog/detail/:id" element={<BlogDetail/>}/>
       <Route path="/shop" element={<Shop/>}></Route>
       <Route path="/login" element={<LoginPage/>}></Route>
-      {/* <Route path="/register" element={<Register/>}></Route> */}
+      <Route path="/register" element={<Register/>}></Route>
       <Route path="/contact" element={<ContactPage/>}></Route>
       <Route path="/cart" element={<Cart/>}></Route>
+      <Route path="/profile" element={<ProfilePage/>}></Route>
+
     </Routes>
   );
 }
