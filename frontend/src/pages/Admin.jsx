cb89ca3001f47d1a34e54fe427c31c1def403306
cuong <cuong1042k1@gmail.com>
2022-10-04 22:29:16 +0700

    cm

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import SideBar from "../components/SideBar/SideBar";
 import DataCustomer from "../components/DataCustomer/DataCustomer";
-import WareHouse from "../components/WareHouse/WareHouse";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import WareHouse from "../components/WareHouse/WareHouse";
 import AddProductContent from "../components/AddProductContent/AddProductContent";
 import Action from "../components/Action/Action";
 function Admin() {
@@ -19,8 +19,8 @@ function Admin() {
           showFilter={false}
           categories={categories}
         />
-        {/* <DataCustomer /> */}
-        <WareHouse></WareHouse>
+        <DataCustomer />
+        {/* <WareHouse></WareHouse> */}
         {/* <AddProductContent/> */}
       </div>
       <Footer />
