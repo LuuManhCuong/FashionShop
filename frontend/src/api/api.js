f56f91a45e6b5bf28dea8b6715999acfbcc66495
cuong <cuong1042k1@gmail.com>
2023-04-02 14:15:37 +0700

    bug

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -8,7 +8,7 @@ export const HOT_URL = "https://fashionshop.onrender.com";
 export const loginUser = async (user, dispatch, navigate) => {
   dispatch(userSlice.actions.loginStart());
   try {
-    const res = await axios.post(`$HOT_URL}/login`, user, {
+    const res = await axios.post(`${HOT_URL}/login`, user, {
       withCredentials: true,
     });
     dispatch(userSlice.actions.loginSuccess(res.data));
@@ -22,7 +22,7 @@ export const loginUser = async (user, dispatch, navigate) => {
 export const registerRequest = async (user, dispatch, navigate) => {
   dispatch(userSlice.actions.registerStart());
   try {
-    await axios.post(`$HOT_URL}/register`, user, {
+    await axios.post(`${HOT_URL}/register`, user, {
       withCredentials: true,
     });
     dispatch(userSlice.actions.registerSuccess());
@@ -32,13 +32,13 @@ export const registerRequest = async (user, dispatch, navigate) => {
   }
 };
 
-export const fetchHomeApi = () => axios.get(`$HOT_URL}`);
-export const fetchBlogApi = () => axios.get(`$HOT_URL}/blog`);
-export const fetchBestSaleApi = () => axios.get(`$HOT_URL}/bestSale/`);
-export const fetchShopApi = () => axios.get(`$HOT_URL}/shop`);
+export const fetchHomeApi = () => axios.get(`${HOT_URL}`);
+export const fetchBlogApi = () => axios.get(`${HOT_URL}/blog`);
+export const fetchBestSaleApi = () => axios.get(`${HOT_URL}/bestSale/`);
+export const fetchShopApi = () => axios.get(`${HOT_URL}/shop`);
 export const fetchAdminApi = () =>
-  axios.get(`$HOT_URL}/admin`, {
+  axios.get(`${HOT_URL}/admin`, {
     withCredentials: true,
   });
 export const fetchSearchApi = (wordKey, size = "less") =>
-  axios.get(`$HOT_URL}/search?name=${wordKey}&size=${size}`);
+  axios.get(`${HOT_URL}/search?name=${wordKey}&size=${size}`);
